90833b9184f341c937b6f5de620b448d42f1539a
Bandics Daniel <126449395+bandics-daniel@users.noreply.github.com>
2025-06-06 10:24:49 +0300

    Scripts

---
+++ Coffee Shop Game 2D/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public int finalScore = 30;
+
+    public TextMeshProUGUI scoreText;
+    public Slider progressBar;
+
+    public TrophyPopup trophyPopup;         // Legătură cu scriptul trofeului
+    private bool trophyShown = false;       // Pentru a evita reapariția
+
+    public void AddPoints(int amount)
+    {
+        score += amount;
+        UpdateUI();
+
+        if (score >= finalScore && !trophyShown)
+        {
+            trophyPopup.ShowTrophy();
+            trophyShown = true;
+        }
+    }
+
+    public void SubtractPoints(int amount)
+    {
+        score -= amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (scoreText != null)
+            scoreText.text = "Scor: " + score;
+
+        if (progressBar != null)
+            progressBar.value = score;
+    }
+}
